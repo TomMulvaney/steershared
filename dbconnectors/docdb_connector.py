34315ae5b46e79235b1f36e2dd52f25f471b94dc
TomMulvaney <mulvaney.tom@gmail.com>
2016-08-24 15:05:07 +0100

    Removed mode from docdb_connector

--- dbconnectors/docdb_connector.py
+++ dbconnectors/docdb_connector.py
@@ -1,5 +1,5 @@
 from FlaskWebProject import app
-from FlaskWebProject.steershared.shared_consts import ID, DEBUG, DB_ID, DOCUMENTDB_HOST, DOCUMENTDB_MASTER_KEY
+from FlaskWebProject.steershared.shared_consts import ID, DB_ID, DOCUMENTDB_HOST, DOCUMENTDB_MASTER_KEY
 import pydocumentdb.errors as errors
 import pydocumentdb.document_client as document_client
 
@@ -23,40 +23,40 @@ def get_client():
     return document_client.DocumentClient(app.config[DOCUMENTDB_HOST], {'masterKey': app.config[DOCUMENTDB_MASTER_KEY]})
 
 
-def get_db_link(mode):
+def get_db_link():
     return 'dbs/{0}_{1}'.format(app.config[DB_ID], mode)
 
 
-def get_collection_link(id_, mode):
-    return get_db_link(mode) + '/colls/{0}'.format(id_)
+def get_collection_link(id_):
+    return get_db_link() + '/colls/{0}'.format(id_)
 
 
-def get_document_link(collection_id, id_, mode):
-    return get_collection_link(collection_id, mode) + '/docs/{0}'.format(id_)
+def get_document_link(collection_id, id_):
+    return get_collection_link(collection_id) + '/docs/{0}'.format(id_)
 
 
 # This is more encapsulated but the introspection feels a bit hacky (see create_doc and create_docs below)
-def create(collection_id, docs, mode=DEBUG):
+def create(collection_id, docs):
     with IDisposable(get_client()) as client:
         created_ids = []
         if type(docs) is dict:
             docs = [docs]
         for doc in docs:
-            new_doc = client.CreateDocument(get_collection_link(collection_id, mode), doc)
+            new_doc = client.CreateDocument(get_collection_link(collection_id), doc)
             created_ids.append(new_doc[ID])
         return created_ids
 
 
-def read(collection_id, query_dicts, mode=DEBUG):
+def read(collection_id, query_dicts):
     with IDisposable(get_client()) as client:
         docs = []
         if query_dicts is None:
-            docs = client.ReadDocuments(get_collection_link(collection_id, mode)).fetch_items()
+            docs = client.ReadDocuments(get_collection_link(collection_id)).fetch_items()
         else:
             for query_dict in query_dicts:
                 try:
                     # TODO: Optimize! If the only query involves an id then use ReadDocument instead
-                    doc = client.QueryDocument(get_collection_link(collection_id, mode), query_dict)
+                    doc = client.QueryDocument(get_collection_link(collection_id), query_dict)
                     docs.append(doc)
                 except errors.DocumentDBError as e:
                     if e.status_code == 404:
@@ -66,16 +66,16 @@ def read(collection_id, query_dicts, mode=DEBUG):
     return docs
 
 
-def update(collection_id, new_docs, mode=DEBUG):
+def update(collection_id, new_docs):
     with IDisposable(get_client()) as client:
         if type(new_docs) is dict:
             new_docs = [new_docs]
         updated_ids = []
         for new_doc in new_docs:
             try:
-                existing_doc = client.ReadDocument(get_document_link(collection_id, new_doc[ID], mode))
+                existing_doc = client.ReadDocument(get_document_link(collection_id, new_doc[ID]))
                 existing_doc.update(new_doc)
-                client.ReplaceDocument(get_document_link(collection_id, existing_doc[ID], mode), existing_doc)
+                client.ReplaceDocument(get_document_link(collection_id, existing_doc[ID]), existing_doc)
                 updated_ids.append(existing_doc[ID])
             except errors.DocumentDBError as e:
                 if e.status_code == 404:
@@ -85,12 +85,12 @@ def update(collection_id, new_docs, mode=DEBUG):
         return updated_ids
 
 
-def delete(collection_id, ids, mode=DEBUG):
+def delete(collection_id, ids):
     with IDisposable(get_client()) as client:
         deleted_ids = []
         for id_ in ids:
             try:
-                client.DeleteDocument(get_document_link(collection_id, id_, mode))
+                client.DeleteDocument(get_document_link(collection_id, id_))
                 deleted_ids.append(id_)
             except errors.DocumentDBError as e:
                 if e.status_code == 404:
