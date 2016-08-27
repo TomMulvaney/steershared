9834b87e5fb93cc705a386a46edb304d73a7301b
TomMulvaney <mulvaney.tom@gmail.com>
2016-08-27 12:37:14 +0100

    Moved str_to_float from db_helpers to converters because of char encoding

---
+++ dbconnectors/converters.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from FlaskWebProject import app
+from db_helpers import get_db
+from FlaskWebProject.steershared.shared_consts import *
+import re
+
+
+def str_to_float(mode, collection_id, attr):
+    db = get_db(mode)
+    docs = db.read(collection_id)
+
+    print docs
+
+    for doc in docs:
+        doc[attr] = doc[attr].replace(u'£', u'').replace('n/a', '-1').replace(',', '').replace('From ', ''). \
+            replace('From: ', '').replace(' / 15 sachets', '').replace('from just ', '').replace('from ', '')
+        doc[attr] = doc[attr].strip()
+
+        if not doc[attr]:
+            doc[attr] = '-1'
+
+        full_hyphen_pattern = '[0-9]+-[0-9]+'
+        half_hyphen_pattern = '[0-9]+-'
+        print doc[attr]
+        if re.match(full_hyphen_pattern, doc[attr]):
+            try:
+                half_match = re.match(half_hyphen_pattern, doc[attr])
+                doc[attr] = re.sub(full_hyphen_pattern, half_match.group(0), doc[attr]).replace('-', '')
+            except Exception as e:
+                print e
+                print doc[attr]
+                raise e
+        try:
+            doc[attr] = float(doc[attr])
+        except Exception as e:
+            print e
+            print doc
+            print doc[attr]
+            raise e
+
+    print docs
+
+    return
+
+    db.update(collection_id, docs)
+
+
+if __name__ == '__main__':
+    str_to_float(app.config[DEFAULT_MODE], PRODUCTS, PRICE)
