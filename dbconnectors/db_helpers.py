9edb4e4645922d60ac15b121808084c2c0ba21db
TomMulvaney <mulvaney.tom@gmail.com>
2016-08-27 12:31:36 +0100

    Added str_to_float function  to db_helpers

--- dbconnectors/db_helpers.py
+++ dbconnectors/db_helpers.py
@@ -1,6 +1,45 @@
 from FlaskWebProject import app
-from FlaskWebProject.steershared.shared_consts import ID, DB_ACCESS_MODULE, MODE_HEADER_KEY, DEFAULT_MODE, EVAL
+from FlaskWebProject.steershared.shared_consts import ID, DB_ACCESS_MODULE, MODE_HEADER_KEY, DEFAULT_MODE
 import importlib
+import re
+
+
+def str_to_float(mode, collection_id, attr):
+    db = get_db(mode)
+    docs = db.read(collection_id)
+
+    print docs
+
+    for doc in docs:
+        doc[attr] = doc[attr].replace(u'£', u'').replace('n/a', '-1').replace(',', '').replace('From ', ''). \
+            replace('From: ', '').replace(' / 15 sachets', '').replace('from just ', '').replace('from ', '')
+        doc[attr] = doc[attr].strip()
+
+        if not doc[attr]:
+            doc[attr] = '-1'
+
+        full_hyphen_pattern = '[0-9]+-[0-9]+'
+        half_hyphen_pattern = '[0-9]+-'
+        print doc[attr]
+        if re.match(full_hyphen_pattern, doc[attr]):
+            try:
+                half_match = re.match(half_hyphen_pattern, doc[attr])
+                doc[attr] = re.sub(full_hyphen_pattern, half_match.group(0), doc[attr]).replace('-', '')
+            except Exception as e:
+                print e
+                print doc[attr]
+                raise e
+        try:
+            doc[attr] = float(doc[attr])
+        except Exception as e:
+            print e
+            print doc
+            print doc[attr]
+            raise e
+
+    print docs
+
+    db.update(collection_id, docs)
 
 
 def rename_attrs(collection_id, attrs, db=None):
