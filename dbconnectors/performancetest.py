5e347800d5b4e3c384e3e0c22bf2f0482adf0744
TomMulvaney <mulvaney.tom@gmail.com>
2016-09-04 14:14:57 +0100

    Bug fix in performancetest

--- dbconnectors/performancetest.py
+++ dbconnectors/performancetest.py
@@ -2,22 +2,26 @@ from FlaskWebProject.steershared.dbconnectors.db_helpers import get_db
 from FlaskWebProject.steershared.shared_consts import *
 from time import time
 
+
 def test(num_docs):
     doc = {IS_FILE_UPLOAD: False, NAME: 'Playstation 4', PRICE: 285.4, QUANTITY: 10,
-            RETAILER_ID: '57c843145c92eb476191d6d9'}
-    docs =  []
-    for i in num_docs:
+           RETAILER_ID: '57c843145c92eb476191d6d9'}
+    docs = []
+    for i in range(num_docs):
         docs.append(doc)
 
     db = get_db(DEBUG)
+    db.delete(BEACONS)
 
     t0 = time()
     ids = db.create(BEACONS, docs)
     t1 = time() - t0
     print('Create {0}: {1}'.format(num_docs, t1))
 
+    read_ids = {ID: id_ for id_ in ids}
+
     t0 = time()
-    docs = db.read(BEACONS, ids)
+    docs = db.read(BEACONS, read_ids)
     t1 = time() - t0
     print('Read {0}: {1}'.format(num_docs, t1))
 
@@ -36,6 +40,6 @@ def test(num_docs):
 
 
 if __name__ == '__main__':
-    counts = [1, 10, 100, 1000, 10000]
+    counts = [1, 2]
     for count in counts:
         test(count)
