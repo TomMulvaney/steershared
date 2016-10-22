4a3d16dd4069426f0c2870f0b56928156982623f
TomMulvaney <mulvaney.tom@gmail.com>
2016-10-22 12:18:20 +0100

    Cleaned and commented connectors

--- dbconnectors/docdb_connector.py
+++ dbconnectors/docdb_connector.py
@@ -4,6 +4,10 @@ import pydocumentdb.errors as errors
 import pydocumentdb.document_client as document_client
 
 
+def gen_connector(mode):
+    return DocdbConnector(mode)
+
+
 class IDisposable:
     """ A context manager to automatically close an object with a close method
     in a with statement. """
@@ -19,83 +23,122 @@ class IDisposable:
         self = None
 
 
-def get_client():
-    return document_client.DocumentClient(app.config[DOCUMENTDB_HOST], {'masterKey': app.config[DOCUMENTDB_MASTER_KEY]})
+class DocdbConnector:
+    """Connector for DocumentDB database"""
+    def __init__(self, mode):
+        self._mode = mode
 
+    @staticmethod
+    def gen_client(self):
+        """Generates db client"""
+        return document_client.DocumentClient(app.config[DOCUMENTDB_HOST],
+                                              {'masterKey': app.config[DOCUMENTDB_MASTER_KEY]})
 
-def get_db_link():
-    return 'dbs/{0}'.format(app.config[DB_ID])
-    # return 'dbs/{0}_{1}'.format(app.config[DB_ID], mode)
+    @staticmethod
+    def get_collection_link(self, id_):
+        """Gets str link for a collection"""
+        return 'dbs/{0}/colls/{1}'.format(app.config[DB_ID], id_)
 
+    @staticmethod
+    def get_document_link(self, collection_id, id_):
+        """Gets str link for a document"""
+        return self.get_collection_link(collection_id) + '/docs/{0}'.format(id_)
 
-def get_collection_link(id_):
-    return get_db_link() + '/colls/{0}'.format(id_)
+    def create(self, collection_id, docs):
+        """Creates docs, returns list of str representing ids of created docs"""
+        with IDisposable(self.gen_client()) as client:
+            created_ids = []
+            # Recommendation Engine may pass a dictionary, so ensure it's wrapped in a list
+            if type(docs) is dict:
+                docs = [docs]
+            for doc in docs:
+                new_doc = client.CreateDocument(self.get_collection_link(collection_id), doc)
+                created_ids.append(new_doc[ID])
+            return created_ids
 
+    def read(self, collection_id, query_dicts):
+        """Reads docs, returns list of dictionaries representing docs"""
+        # query_dicts are dictionaries which represent queries.
+        # E.g. { NAME: 'foo' } would return all docs where NAME='foo'
+        with IDisposable(self.gen_client()) as client:
+            docs = []
+            # If there are no query dicts then find all docs in the collection
+            if query_dicts is None:
+                docs = client.ReadDocuments(self.get_collection_link(collection_id)).fetch_items()
+            # Else find all docs that match at least one of the query dicts
+            # N.B. We allow multiple query dicts so that they can search for multiple ids [{ID: 'foo'}, {ID: 'bar'}...]
+            else:
+                # Recommendation Engine may pass a dictionary, so ensure it's wrapped in a list
+                if type(query_dicts) is dict:
+                    query_dicts = [query_dicts]
 
-def get_document_link(collection_id, id_):
-    return get_collection_link(collection_id) + '/docs/{0}'.format(id_)
+                for query_dict in query_dicts:
+                    try:
+                        # TODO: Optimize! If the only query involves an id then use ReadDocument instead
+                        doc = client.QueryDocument(self.get_collection_link(collection_id), query_dict)
+                        docs.append(doc)
+                    except errors.DocumentDBError as e:
+                        if e.status_code == 404:
+                            print('A {0} with id \'{1}\' does not exist'.format(collection_id, query_dict))
+                        else:
+                            raise errors.HTTPFailure(e.status_code)
+        return docs
 
-
-# This is more encapsulated but the introspection feels a bit hacky (see create_doc and create_docs below)
-def create(collection_id, docs):
-    with IDisposable(get_client()) as client:
-        created_ids = []
-        if type(docs) is dict:
-            docs = [docs]
-        for doc in docs:
-            new_doc = client.CreateDocument(get_collection_link(collection_id), doc)
-            created_ids.append(new_doc[ID])
-        return created_ids
-
-
-def read(collection_id, query_dicts):
-    with IDisposable(get_client()) as client:
-        docs = []
-        if query_dicts is None:
-            docs = client.ReadDocuments(get_collection_link(collection_id)).fetch_items()
-        else:
-            for query_dict in query_dicts:
+    def update(self, collection_id, new_docs):
+        """Updates docs, returns list of str representing ids of updated docs"""
+        with IDisposable(self.gen_client()) as client:
+            # Recommendation Engine may pass a dictionary, so ensure it's wrapped in a list
+            if type(new_docs) is dict:
+                new_docs = [new_docs]
+            updated_ids = []
+            for new_doc in new_docs:
                 try:
-                    # TODO: Optimize! If the only query involves an id then use ReadDocument instead
-                    doc = client.QueryDocument(get_collection_link(collection_id), query_dict)
-                    docs.append(doc)
+                    # DocumentDB client has no update method, so we need to read and then replace
+                    existing_doc = client.ReadDocument(self.get_document_link(collection_id, new_doc[ID]))
+                    existing_doc.update(new_doc)
+                    client.ReplaceDocument(self.get_document_link(collection_id, existing_doc[ID]), existing_doc)
+                    updated_ids.append(existing_doc[ID])
                 except errors.DocumentDBError as e:
                     if e.status_code == 404:
-                        print('A {0} with id \'{1}\' does not exist'.format(collection_id, query_dict))
+                        print('A {0} with id \'{1}\' does not exist'.format(collection_id, new_doc[ID]))
                     else:
                         raise errors.HTTPFailure(e.status_code)
-    return docs
+            return updated_ids
 
+    def replace(self, collection_id, replace_docs):
+        """Replaces docs, returns list of str representing ids of replaced docs"""
+        with IDisposable(self.gen_client()) as client:
+            # Recommendation Engine may pass a dictionary, so ensure it's wrapped in a list
+            if type(replace_docs) is dict:
+                replace_docs = [replace_docs]
 
-def update(collection_id, new_docs):
-    with IDisposable(get_client()) as client:
-        if type(new_docs) is dict:
-            new_docs = [new_docs]
-        updated_ids = []
-        for new_doc in new_docs:
-            try:
-                existing_doc = client.ReadDocument(get_document_link(collection_id, new_doc[ID]))
-                existing_doc.update(new_doc)
-                client.ReplaceDocument(get_document_link(collection_id, existing_doc[ID]), existing_doc)
-                updated_ids.append(existing_doc[ID])
-            except errors.DocumentDBError as e:
-                if e.status_code == 404:
-                    print('A {0} with id \'{1}\' does not exist'.format(collection_id, new_doc[ID]))
-                else:
-                    raise errors.HTTPFailure(e.status_code)
-        return updated_ids
+            replace_ids = []
 
+            for replace_doc in replace_docs:
+                try:
+                    client.ReplaceDocument(self.get_document_link(collection_id, replace_doc[ID]), replace_doc)
+                    replace_ids.append(replace_doc[ID])
+                except Exception as e:
+                    print type(e)
+                    print e
+                    raise e
+            return replace_ids
 
-def delete(collection_id, ids):
-    with IDisposable(get_client()) as client:
-        deleted_ids = []
-        for id_ in ids:
-            try:
-                client.DeleteDocument(get_document_link(collection_id, id_))
-                deleted_ids.append(id_)
-            except errors.DocumentDBError as e:
-                if e.status_code == 404:
-                    print('A {0} with id \'{1}\' does not exist'.format(collection_id, id_))
-                else:
-                    raise errors.HTTPFailure(e.status_code)
-    return deleted_ids
+    def delete(self, collection_id, ids):
+        """Deletes docs, returns list of str representing ids of deleted docs"""
+        with IDisposable(self.gen_client()) as client:
+            # Recommendation Engine may pass a str, so ensure it's wrapped in a list
+            if type(ids) is str:
+                ids = [ids]
+
+            deleted_ids = []
+            for id_ in ids:
+                try:
+                    client.DeleteDocument(self.get_document_link(collection_id, id_))
+                    deleted_ids.append(id_)
+                except errors.DocumentDBError as e:
+                    if e.status_code == 404:
+                        print('A {0} with id \'{1}\' does not exist'.format(collection_id, id_))
+                    else:
+                        raise errors.HTTPFailure(e.status_code)
+        return deleted_ids
--- dbconnectors/mongodb_connector.py
+++ dbconnectors/mongodb_connector.py
@@ -5,11 +5,13 @@ from bson.objectid import ObjectId
 from copy import deepcopy
 from functools import wraps
 
-
+# Mongo uses '_id' instead of 'id' attribute. This detail is abstracted away from clients.
+# All Spot Markets docs use 'id' attribute.
 _mongo_id_key = '_id'
 
 
 def _get_mongo_id(id_):
+    """Transforms id string in mongo id obj"""
     return ObjectId(id_)
 
 
@@ -27,41 +29,46 @@ def gen_connector(mode):
 
 
 def deco_retry(f):
+    """Function wrapper for all MongoConnector CRUD operations. Reinstantiates db client if operation fails"""
     @wraps(f)
     def f_retry(self, *args, **kwargs):
         mtries = 2
         while mtries > 1:
+            # Try to perform the operation
             try:
                 return f(self, *args, **kwargs)
+            # Reinstantiate db client if exception is raised
             except Exception as e:
-                # Reinstantiate db client
-                self.gen_db()
+                self.gen_client()
                 print type(e)
                 print e
             print 'Retrying mongodbconnector'
             mtries -= 1
+        # This will execute as the final try, if this call fails then the operation will fail
         return f(self, *args, **kwargs)
 
     return f_retry  # true decorator
 
 
 class MongoConnector:
+    """Connector for MongoDB database"""
     def __init__(self, mode):
         self._mode = mode
-        self._db = None
-        self.gen_db()
+        self._client = None
+        self.gen_client()
 
-    def gen_db(self):
-        self._db = MongoClient(app.config[MONGO_DB])['{0}_{1}'.format(app.config[DB_ID], self._mode)]
+    def gen_client(self):
+        """Generates db client"""
+        self._client = MongoClient(app.config[MONGO_DB])['{0}_{1}'.format(app.config[DB_ID], self._mode)]
 
     @deco_retry
     def create(self, collection_id, docs):
-        # Machine Learning may call this function directly and pass a dictionary, so make sure that it's wrapped in a
-        # list
+        """Creates docs, returns list of str representing ids of created docs"""
+        # Recommendation Engine may pass a dictionary, so ensure it's wrapped in a list
         if type(docs) is dict:
             docs = [docs]
         try:
-            results = self._db[collection_id].insert_many(docs)
+            results = self._client[collection_id].insert_many(docs)
             ids = [str(inserted_id) for inserted_id in results.inserted_ids]
             for doc in docs:
                 del doc[_mongo_id_key]
@@ -73,20 +80,28 @@ class MongoConnector:
 
     @deco_retry
     def read(self, collection_id, query_dicts=None):
-        # Machine Learning may call this function directly and pass a dictionary, so ensure it's wrapped in a list
+        """"Reads docs, returns list of dictionaries representing docs"""
+        # query_dicts are dictionaries which represent queries.
+        # E.g. { NAME: 'foo' } would return all docs where NAME='foo'
+
+        # Recommendation Engine may pass a dictionary, so ensure it's wrapped in a list
         if type(query_dicts) is dict:
             query_dicts = [query_dicts]
+
         try:
+            # If there are no query dicts then find all docs in the collection
             if not query_dicts:
-                cursor = self._db[collection_id].find()
+                cursor = self._client[collection_id].find()
                 return _extract_docs(cursor)
+            # Else find all docs that match at least one of the query dicts
+            # N.B. We allow multiple query dicts so that they can search for multiple ids [{ID: 'foo'}, {ID: 'bar'}...]
             else:
                 docs = []
                 for query_dict in query_dicts:
                     if ID in query_dict.keys():
                         query_dict[_mongo_id_key] = ObjectId(query_dict[ID])
                         del query_dict[ID]
-                    cursor = self._db[collection_id].find(query_dict)
+                    cursor = self._client[collection_id].find(query_dict)
                     docs.extend(_extract_docs(cursor))
                 return docs
         except Exception as e:
@@ -96,10 +111,13 @@ class MongoConnector:
 
     @deco_retry
     def update(self, collection_id, updated_docs):
-        # Machine Learning may call this function directly and pass a dictionary, so ensure it's wrapped in a list
+        """Updates docs, returns list of str representing ids of updated docs"""
+        # Recommendation Engine may call this function directly and pass a dictionary, so ensure it's wrapped in a list
         if type(updated_docs) is dict:
             updated_docs = [updated_docs]
 
+        # Deepy copy because the doc has 'id' but mongo uses '_id', so attr names are going to change but we want
+        # to abstract this detail away from the caller
         updated_docs = deepcopy(updated_docs)
 
         updated_ids = []
@@ -107,7 +125,7 @@ class MongoConnector:
             try:
                 id_ = updated_doc[ID]
                 updated_doc = {'$set': updated_doc}
-                self._db[collection_id].update_one({_mongo_id_key: ObjectId(id_)}, updated_doc)
+                self._client[collection_id].update_one({_mongo_id_key: ObjectId(id_)}, updated_doc)
                 updated_ids.append(id_)
             except Exception as e:
                 print type(e)
@@ -117,17 +135,20 @@ class MongoConnector:
 
     @deco_retry
     def replace(self, collection_id, replace_docs):
-        # Machine Learning may call this function directly and pass a dictionary, so ensure it's wrapped in a list
+        """Replaces docs, returns list of str representing ids of replaced docs"""
+        # Recommendation Engine may pass a dictionary, so ensure it's wrapped in a list
         if type(replace_docs) is dict:
             replace_docs = [replace_docs]
 
+        # Copy because the doc has 'id' but mongo uses '_id', so attr names are going to change but we want
+        # to abstract this detail away from the caller
         replace_docs = deepcopy(replace_docs)
 
         replace_ids = []
         for replace_doc in replace_docs:
             try:
                 id_ = replace_doc[ID]
-                self._db[collection_id].replace_one({_mongo_id_key: ObjectId(id_)}, replace_doc)
+                self._client[collection_id].replace_one({_mongo_id_key: ObjectId(id_)}, replace_doc)
                 replace_ids.append(id_)
             except Exception as e:
                 print type(e)
@@ -137,16 +158,17 @@ class MongoConnector:
 
     @deco_retry
     def delete(self, collection_id, ids=None):
-        # Machine Learning may call this function directly and pass a string, so make sure that it's wrapped in a list
+        """Deletes docs, returns list of str representing ids of deleted docs"""
+        # Recommendation Engine may pass a string, so ensure it's wrapped in a list
         if not ids:
-            self._db[collection_id].delete_many({})
+            self._client[collection_id].delete_many({})
             return []
         else:
             if type(ids) is str:
                 ids = [ids]
             for id_ in ids:
                 try:
-                    self._db[collection_id].delete_one({_mongo_id_key: ObjectId(id_)})
+                    self._client[collection_id].delete_one({_mongo_id_key: ObjectId(id_)})
                 except Exception as e:
                     print type(e)
                     print e
